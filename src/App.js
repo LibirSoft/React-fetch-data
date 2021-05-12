b9603d4c278ba50f1440a862359a67bcd12125ff
LibirSoft <libir@outlook.com>
2021-05-13 02:34:02 +0300

    loading screen added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import "./App.css"
 export default class App extends Component {
   state = {
     users: [],
+    isloading:true
   };
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((data) => data.json())
-      .then((users) => {
-        this.setState({ users: users });
-      });
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((data) => data.json())
+        .then((users) => {
+          this.setState({
+            users: users,
+            isloading: false
+
+          });
+        });
+    },3000)
+
   }
 
   render() {
+    const { isloading } = this.state;
+    
     return (
       <div className="App">
         <h1>Users</h1>
+        {isloading ? "Loading...": ""}
         {this.state.users.map((user) => (
           <div key={user.id}>{user.name}</div>
         ))}
